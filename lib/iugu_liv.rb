716656a2df69f01090199c9c6c0a2d9afd33db8c
RobertozsCunha <robertocunha@poli.ufrj.br>
2018-02-28 13:23:18 -0300

    methods

--- lib/iugu_liv.rb
+++ lib/iugu_liv.rb
@@ -1,5 +1,27 @@
 require "iugu_liv/version"
 
 module IuguLiv
-  
+  def gerar_token
+  end
+
+  def criar_usuario
+  end
+
+  def alterar_usuario
+  end
+
+  def criar_forma_pagamento
+  end
+
+  def alterar_forma_pagamento
+  end
+
+  def criar_assinatura
+  end
+
+  def alterar_assinatura
+  end
+
+  def cancelar_assinatura
+  end
 end
